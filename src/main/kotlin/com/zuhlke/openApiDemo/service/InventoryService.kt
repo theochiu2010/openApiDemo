8a8e79a90dcda5fda795e0063244d03a782e5802
theochiu2010 <alanchiu3730@gmail.com>
2020-11-03 16:25:30 +0800

    generated swagger code gen code

--- src/main/kotlin/com/zuhlke/openApiDemo/service/InventoryService.kt
+++ src/main/kotlin/com/zuhlke/openApiDemo/service/InventoryService.kt
@@ -1,29 +1,29 @@
 package com.zuhlke.openApiDemo.service
 
-import com.zuhlke.openApiDemo.controller.InventoryApiDelegate
-import com.zuhlke.openApiDemo.model.InventoryItem
-import com.zuhlke.openApiDemo.model.Manufacturer
-import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Service
-import java.time.OffsetDateTime
-import java.util.UUID
+//import com.zuhlke.openApiDemo.controller.InventoryApiDelegate
+//import com.zuhlke.openApiDemo.model.InventoryItem
+//import com.zuhlke.openApiDemo.model.Manufacturer
+//import org.springframework.http.ResponseEntity
+//import org.springframework.stereotype.Service
+//import java.time.OffsetDateTime
+//import java.util.UUID
 
-@Service
-class InventoryService: InventoryApiDelegate {
-    override fun searchInventory(searchString: String?, skip: Int?, limit: Int?): ResponseEntity<MutableList<InventoryItem>> {
-        val result = mutableListOf(
-                InventoryItem().apply {
-                    id = UUID.randomUUID()
-                    name = "chocolate"
-                    releaseDate = OffsetDateTime.now()
-                    manufacturer = Manufacturer().apply {
-                        name = "HKMC"
-                        homePage = "google.com"
-                        phone = "12345678"
-                    }
-                }
-        )
-
-        return ResponseEntity.ok(result)
-    }
-}
+//@Service
+//class InventoryService: InventoryApiDelegate {
+//    override fun searchInventory(searchString: String?, skip: Int?, limit: Int?): ResponseEntity<MutableList<InventoryItem>> {
+//        val result = mutableListOf(
+//                InventoryItem().apply {
+//                    id = UUID.randomUUID()
+//                    name = "chocolate"
+//                    releaseDate = OffsetDateTime.now()
+//                    manufacturer = Manufacturer().apply {
+//                        name = "HKMC"
+//                        homePage = "google.com"
+//                        phone = "12345678"
+//                    }
+//                }
+//        )
+//
+//        return ResponseEntity.ok(result)
+//    }
+//}
